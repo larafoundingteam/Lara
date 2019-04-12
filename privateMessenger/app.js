431083f6149d850a5fc64f423c411a5e31e5048f
alexwaibel <al3x.waib3l@gmail.com>
2019-04-12 10:43:15 -0400

    Create test post route.

--- privateMessenger/app.js
+++ privateMessenger/app.js
@@ -22,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.post('/', function (req, res) {
+    res.send('Got a POST request')
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
