115ae698ccd22d227a0e93e656227ef57bcd6673
shoafj7917 <shoafj7917@gmail.com>
2019-05-08 02:09:39 -0400

    Changed logic to only authorized user admin to be used for integration testing

--- source/routes/users.js
+++ source/routes/users.js
@@ -3,8 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource or user data');
+router.post('/', (req, res, next) => {
+  if (req.body.username === undefined) {
+    res.statusMessage = 'Username must be specified';
+    res.status(400).end();
+  }
+  if (req.body.username.toLowerCase().trim() === 'admin') {
+    res.send('Authorized');
+  } else {
+    res.statusMessage =
+      'Unauthorized you are not the user we are looking for... Try another username';
+    res.status(400).end();
+  }
 });
 
 module.exports = router;
