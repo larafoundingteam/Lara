62151e1b90627c2c1c129c1ea16ba594c860a2b8
shoafj7917 <shoafj7917@gmail.com>
2019-05-11 01:00:42 -0400

    testing that bad test fails pipeline

--- source/test/authenticatorTests.js
+++ source/test/authenticatorTests.js
@@ -12,7 +12,7 @@ describe('Users Route Unit Tests', () => {
     });
 
     it('returns False for invalidUser', done => {
-      expect(user.isAuthorizedUser('UnknownUser')).to.be.equal(false);
+      expect(user.isAuthorizedUser('UnknownUser')).to.be.equal(true);
       done();
     });
   });
